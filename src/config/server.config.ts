808560da08212f937e44c6f91b110e28a0025086
Enrique Marin Hirales <enrique_marin_h@hotmail.com>
2025-08-25 14:56:45 -0700

    feat: add localhost:4173 to allowed CORS origins

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -3,7 +3,7 @@ export default () => ({
   isDev: process.env.NODE_ENV !== 'production',
   port: parseInt(process.env.PORT || '3333') || 3333,
   secretKey: process.env.SECRET_KEY || 'secret',
-  origins: process.env.CORS_ORIGIN?.split('|') || ['http://localhost:8080'],
+  origins: process.env.CORS_ORIGIN?.split('|') || ['http://localhost:8080', 'http://localhost:4173'],
   cookieMaxAge: process.env.COOKIE_MAX_AGE || 24 * 60 * 60 * 1000,
   fileHost: process.env.FILE_HOST || `localhost:${process.env.PORT || 3333}`,
   fileProtocol: process.env.FILE_PROTOCOL || 'http',
